553000260bf6a48ce1222e70e702f02054092ca3
Anabella Lamour <ana.lamour@outlook.com>
2021-11-03 17:58:40 -0300

    js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log("iniciar nuestro js");
+let array_colores = new Array("btn-info", "btn-secondary", "btn-dark");
+
+function colorCard() {
+    let botones = document.getElementsByClassName("btn"); //html collecctions
+    let array_botones = Array.prototype.slice.call(botones); //html en array
+    array_botones.forEach(function(element, indice, array) {
+        console.log(element);
+        element.classList.add(array_colores[indice]);
+
+    });
+}
