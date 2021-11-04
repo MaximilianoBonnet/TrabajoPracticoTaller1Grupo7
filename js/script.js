979ed56a473d6d826eb54bf5f5d2f282c66e9f97
Anabella Lamour <ana.lamour@outlook.com>
2021-11-03 21:57:00 -0300

    js botones

--- js/script.js
+++ js/script.js
@@ -5,13 +5,31 @@ function colorCard(estado) {
     //primero defino botones
     let botones = document.getElementsByClassName("btn"); //html collecctions
     let array_botones = Array.prototype.slice.call(botones); //html en array
-    if (estado == true) {
-        array_botones.forEach(function(element, indice, array) {
-            console.log(element);
-            element.classList.add(array_colores[indice]);
+    var count_array = array_colores.length - 1;
+    var i = 0;
+    array_botones.forEach(function(element, indice, array) {
 
-        });
+        if (estado == true) {
+            element.classList.add("btn-primary");
+            element.classList.add(array_colores[i]);
+
+        } else {
+            element.classList.remove(array_colores[indice]);
+            element.classList.add("btn-primary");
+        }
+        if (i == count_array) {
+            i = 0;
+        } else {
+            i++;
+        }
+    });
+}
+
+function mostrarOcultar(show) {
+    let row = document.getElementById("mostrarOcultar");
+    if (show == true) {
+        row.classList.remove("d-none";)
+    } else {
+        row.classList.add("d-none");
     }
-
-
 }
