2ede2a48d6ad192ca143b16bd4d2aa6fef3ca038
Anabella Lamour <ana.lamour@outlook.com>
2021-11-13 14:25:51 -0300

    alert modal contact

--- js/script.js
+++ js/script.js
@@ -1,35 +1,19 @@
-/*console.log("iniciar nuestro js");
-let array_colores = new Array("btn-info", "btn-secondary", "btn-dark");
+console.log("mi página");
 
-function colorCard(estado) {
-    //primero defino botones
-    let botones = document.getElementsByClassName("btn"); //html collecctions
-    let array_botones = Array.prototype.slice.call(botones); //html en array
-    var count_array = array_colores.length - 1;
-    var i = 0;
-    array_botones.forEach(function(element, indice, array) {
+let test = ' ';
 
-        if (estado == true) {
-            element.classList.add("btn-primary");
-            element.classList.add(array_colores[i]);
 
-        } else {
-            element.classList.remove(array_colores[indice]);
-            element.classList.add("btn-primary");
-        }
-        if (i == count_array) {
-            i = 0;
-        } else {
-            i++;
-        }
-    });
-}
-
-function mostrarOcultar(show) {
-    let row = document.getElementById("mostrarOcultar");
-    if (show == true) {
-        row.classList.remove("d-none");
-    } else {
-        row.classList.add("d-none");
+function enviarDatos() {
+    let nombre = document.getElementById("nombre");
+    let apellido = document.getElementById("apellido");
+    let email = document.getElementById("email");
+    let telefono = document.getElementById("telefono");
+    let textarea = document.getElementById("info")
+    if (email.value == '') {
+        alert("Falta ingresar datos");
     }
-}*/
+    if (textarea == '') {
+        alert("Falta ingresar datos \n");
+    }
+    alert("Mensaje enviado con éxito. \n Nos pondremos en contacto a la brevedad");
+}
