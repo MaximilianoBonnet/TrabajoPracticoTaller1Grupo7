12a770c5b9b51db6bce7334700436657363ce03d
estebanferrari86 <estebanalejandroferrari@gmail.com>
2021-12-10 18:58:39 -0300

    ordenamiento version 1

--- js/funciones.js
+++ js/funciones.js
@@ -14,7 +14,6 @@ async function sha256(message) {
     return hashHex;
 }
 
-
 function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
@@ -23,3 +22,114 @@ function getParameterByName(name) {
 }
 
 let params = new URLSearchParams(location.search);
+
+function setTablaDestacados() {
+    let destacados = productos.cursos;
+    destacados.forEach(element => {
+        if (element.cursoDestacado == true) {
+
+            let elementGalery = '<div class="col">';
+            elementGalery += '<div class="card shadow-sm" id="card' + element.id + '">';
+            elementGalery += '<title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect>';
+            elementGalery += '<img src="' + element.imagen + '" class="card-img-top" alt="...">';
+            elementGalery += '<div class="card-body">';
+            elementGalery += '<p class="card-text"><b> NOMBRE: </b>' + element.nombre + ' </p>';
+            elementGalery += '<p class="card-text"><b> PRECIO: </b>' + element.precio + ' </p>';
+            elementGalery += '<p class="card-text"><b> DESCRIPCION: </b>' + element.descripcion + ' </p>';
+            elementGalery += '<div class="d-flex justify-content-between align-items-center">';
+            elementGalery += ' <div class="btn-group">';
+            elementGalery += '<button type="button" class="btn btn-dark"> <center> <a href="' + element.pagina + '">Ver Mas</a></button></center>';
+            elementGalery += '</div></div></div></div></div>';
+
+            let gallery = document.getElementById('gallery_view');
+            $('#gallery_view').append(elementGalery);
+
+            console.log(element);
+        }
+    });
+}
+
+function setCursos() {
+    let listaCursos = productos.cursos;
+    listaCursos.forEach(element => {
+        if (element.cursoDestacado == false) {
+
+            let elementListaCursos = '<div class="col-2">';
+            elementListaCursos += '<div  class="cards" id="card' + element.id + '">';
+            elementListaCursos += '<img src="' + element.imagen + '" class="card-img-top" alt="' + element.imagen + '">';
+            elementListaCursos += '<div class="card-body">';
+            elementListaCursos += '<h5 class="card-text">' + element.nombre + ' </h5>';
+            elementListaCursos += '<p class="card-title">' + element.descripcion + '</p>';
+            elementListaCursos += '<p>' + element.precio + '</p>' + '<center> <a href="contacto.html" class="btn btn-dark">Contacto</a></center>';
+            elementListaCursos += '</div></a></div></div>';
+            let gallery = document.getElementById('listaCursos');
+            $('#listaCursos').append(elementListaCursos);
+
+            console.log(element);
+        }
+    });
+}
+
+function sortJSON(data, key, orden) {
+    return data.sort(function(a, b) {
+        var x = a[key],
+            y = b[key];
+
+        if (orden === 'asc') {
+            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+        }
+
+        if (orden === 'desc') {
+            return ((x > y) ? -1 : ((x < y) ? 1 : 0));
+        }
+    });
+}
+
+function setOrdenarAZ() {
+
+    let listaCursos = productos.cursos;
+    var oJSON = sortJSON(listaCursos, 'nombre', 'asc');
+    oJSON.forEach(element => {
+        if (element.cursoDestacado == false) {
+
+            let elementListaCursos = '<div class="col-2">';
+            elementListaCursos += '<div  class="cards" id="card' + element.id + '">';
+            elementListaCursos += '<img src="' + element.imagen + '" class="card-img-top" alt="' + element.imagen + '">';
+            elementListaCursos += '<div class="card-body">';
+            elementListaCursos += '<h5 class="card-text">' + element.nombre + ' </h5>';
+            elementListaCursos += '<p class="card-title">' + element.descripcion + '</p>';
+            elementListaCursos += '<p>' + element.precio + '</p>' + '<center> <a href="contacto.html" class="btn btn-dark">Contacto</a></center>';
+            elementListaCursos += '</div></a></div></div>';
+
+            let gallery = document.getElementById('ordenarAZ');
+            $('#ordenarAZ').append(elementListaCursos);
+
+            console.log(element);
+        }
+    });
+}
+
+function setOrdenarZA() {
+
+    let listaCursos = productos.cursos;
+    var oJSON = sortJSON(listaCursos, 'nombre', 'desc');
+    oJSON.forEach(element => {
+        if (element.cursoDestacado == false) {
+
+
+            let elementListaCursos = '<div class="col-2">';
+            elementListaCursos += '<div  class="cards" id="card' + element.id + '">';
+            elementListaCursos += '<img src="' + element.imagen + '" class="card-img-top" alt="' + element.imagen + '">';
+            elementListaCursos += '<div class="card-body">';
+            elementListaCursos += '<h5 class="card-text">' + element.nombre + ' </h5>';
+            elementListaCursos += '<p class="card-title">' + element.descripcion + '</p>';
+            elementListaCursos += '<p>' + element.precio + '</p>' + '<center> <a href="contacto.html" class="btn btn-dark">Contacto</a></center>';
+            elementListaCursos += '</div></a></div></div>';
+
+            let gallery = document.getElementById('ordenarZA');
+            $('#ordenarZA').append(elementListaCursos);
+
+            console.log(element);
+        }
+    });
+}
