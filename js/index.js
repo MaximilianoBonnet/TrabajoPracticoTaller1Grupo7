840f138086eb9647f4eec48b6a51c68d48304088
estebanferrari86 <estebanalejandroferrari@gmail.com>
2021-12-09 22:41:25 -0300

    json productos javascript index y correciones

--- js/index.js
+++ js/index.js
@@ -1,65 +1,38 @@
-const url = 'productos.json';
+const url = '../TrabajoPracticoTaller1Grupo7/productos.json';
 const request = new XMLHttpRequest();
 request.open('GET', url); // setean el método, la url de api
 request.responseType = 'json'; //definen el tipo de dato que les devuelve
 request.send(); //envían la solicitud
 var productos = '';
 
-console.log(request.response);
-
-
-
-function setDataCursos() {
-    let curso = document.getElementById("");
-    curso.innerHTML = .dt;
-    let titulo = document.getElementById("title");
-    titulo.innerHTML = "<b>" + equipo["pais"] + "</b>";
-    let grupo = document.getElementById("grupo");
-    grupo.innerHTML = equipo.grupo;
-
-
+request.onload = function() {
+    console.log(request.response);
+    productos = request.response;
+    setTablaDestacados(); //funcion que trae productos destacados
 }
 
-$(document).ready(function() {
+function setTablaDestacados() {
+    let destacados = productos.cursos;
+    destacados.forEach(element => {
+        if (element.cursoDestacado == true) {
 
-    $('#addPlayer').submit(function(e) {
-        e.preventDefault();
-        console.log("ENVIADO FORMULARIO")
-    })
-});
+            let elementGalery = '<div class="col">';
+            elementGalery += '<div class="card shadow-sm" id="card' + element.id + '">';
+            elementGalery += '<title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect>';
+            elementGalery += '<img src="' + element.imagen + '" class="card-img-top" alt="...">';
+            elementGalery += '<div class="card-body">';
+            elementGalery += '<p class="card-text"><b> NOMBRE: </b>' + element.nombre + ' </p>';
+            elementGalery += '<p class="card-text"><b> PRECIO: </b>' + element.precio + ' </p>';
+            elementGalery += '<p class="card-text"><b> DESCRIPCION: </b>' + element.descripcion + ' </p>';
+            elementGalery += '<div class="d-flex justify-content-between align-items-center">';
+            elementGalery += '<div class="btn-group">';
+            elementGalery += '<button type="button" class="btn btn-sm btn-dark colorVerdeBoton"> <a href="' + element.pagina + '">Ver Mas</a></button>';
+            elementGalery += '</div></div></div></div></div>';
 
-function enviarForm() {
-    $('#addPlayer').submit();
-}
+            let gallery = document.getElementById('gallery_view');
+            $('#gallery_view').append(elementGalery);
 
-function setTableGammers() {
-    let jugadores = equipo.jugadores;
-    jugadores.forEach(element => {
-
-
-        let elementGalery = '<div class="col"><div class="card shadow-sm" id="card' + element.id + '">';
-        elementGalery += '<svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>';
-        elementGalery += '<div class="card-body">';
-        elementGalery += '<p class="card-text"><b> NOMBRE: ' + element.nombre + ' </b></p>';
-        elementGalery += '<p class="card-text"><b> POSICION: </b>' + element.posicion + ' </p>';
-        elementGalery += '<p class="card-text"><b> APODO: </b>' + element.apodo + ' </p>';
-        elementGalery += '<p class="card-text"><b> EDAD: </b>' + element.edad + ' </p>';
-        elementGalery += '<div class="d-flex justify-content-between align-items-center">';
-        elementGalery += '<div class="btn-group">';
-        elementGalery += '<button type="button" class="btn btn-sm btn-outline-secondary" onclick="viewPlayer(\'' + element + '\')">View</button>';
-        elementGalery += '<button type="button" class="btn btn-sm btn-outline-secondary" onclick="showAlert(' + element.id + ', true)">Alert</button>';
-        elementGalery += '<button type="button" class="btn btn-sm btn-outline-secondary" onclick="showAlert(' + element.id + ', false)">remove Alert</button>';
-        elementGalery += '</div><small class="text-muted">9 mins</small></div></div></div></div>';
-
-        let gallery = document.getElementById('gallery_view');
-        // gallery.innerHTML = elementGalery;
-        $('#gallery_view').append(elementGalery);
-
-        console.log(element);
+            console.log(element);
+        }
     });
-}
-
-
-function viewPlayer(player) {
-    console.log(Object.values(player));
 }
